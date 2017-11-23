597f241e1cd1a8d140b61e00ce1ac8157864f316
finapp <dev1@bizpark.co>
2017-11-23 16:08:07 +0100

    Added part of email validate

--- GenerationgOffers/Controllers/HomeController.cs
+++ GenerationgOffers/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenerationgOffers.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,15 @@ namespace GenerationgOffers.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly EmailService _emailService;
+        public HomeController()
+        {
+            _emailService = new EmailService();
+        }
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            _emailService.ValidateEmail("das");
             return View();
         }
     }
--- GenerationgOffers/Services/EmailService.cs
+++ GenerationgOffers/Services/EmailService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace GenerationgOffers.Services
@@ -14,12 +15,44 @@ namespace GenerationgOffers.Services
             HtmlDocument document = new HtmlDocument();
             document.OptionOutputAsXml = true;
             document.LoadHtml(email);
-
+            string t = "Dane klienta: Kwota limitu zł 50000   Email   katrans@katrans.pl      Firma   KATRANS SP. Z O.O.      Telefon 791869376       Branża  transport       Sprzedaż        2.000.000       Strona produktu http://www.smartfaktor.pl/produkty/faktoring/wycena";
             HtmlNode node = document.DocumentNode;
 
-            
+            string text = document.DocumentNode.InnerText;
+            var words = GetWordsFromEmail(t);
 
+            int limitAmount;
+            string emailName;
+            string firmName;
+            string telNumber;
+            string trade;
+            string amountString;
+
+            //foreach (var word in words)
+            //{
+            //    if()
+            //}
             return null;
+        }
+
+        public IEnumerable<string> GetWordsFromEmail(string email)
+        {
+            MatchCollection matches = Regex.Matches(email, @"\b[\w']*\b");
+
+            return from m in matches.Cast<Match>()
+                   where !string.IsNullOrEmpty(m.Value)
+                   select TrimSuffix(m.Value);
+        }
+
+        static string TrimSuffix(string word)
+        {
+            int apostropheLocation = word.IndexOf('\'');
+            if (apostropheLocation != -1)
+            {
+                word = word.Substring(0, apostropheLocation);
+            }
+
+            return word;
         }
 
     }
